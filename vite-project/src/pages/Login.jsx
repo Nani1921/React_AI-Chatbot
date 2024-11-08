8408ca9daf2dc20f435f3bc40baf9438f715b7f5
Dasaradha Rami Reddy <140581662+Nani1921@users.noreply.github.com>
2024-11-08 19:20:55 +0530

    Login.jsx

--- vite-project/src/pages/Login.jsx
+++ vite-project/src/pages/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:5000/user/login", formData);
+      const response = await axios.post("https://react-ai-chatbot-eta.vercel.app/user/login", formData);
       setMessage("Login successful");
       localStorage.setItem("token", response.data.token);
       navigate("/dashboard/userhome");
